6dd8e8b42cf29dd556009f644b3cc34f33b69f6b
sk <sk@bitsandlikes.de>
2022-12-02 19:14:09 +0100

    day01 part one

--- day01/mod.rs
+++ day01/mod.rs
@@ -1,5 +1,15 @@
-pub fn part_one(input: &str) -> i64 {
-    return -1;
+pub fn part_one(input: &str) -> usize {
+    let mut max: usize = 0;
+    for chunk in input.trim().split("\n\n") {
+        let sum: usize = chunk.trim()
+            .split("\n")
+            .map(|x| x.parse::<usize>().unwrap()).sum();
+        if max < sum {
+            max = sum;
+        }
+    }
+    println!("day01 -> part one: {}", max);
+    return max;
 }
 pub fn part_two(input: &str) -> i64 {
     return -1;
@@ -9,7 +19,7 @@ pub fn part_two(input: &str) -> i64 {
 mod tests {
     #[test]
     fn part_one() {
-        assert_eq!(super::part_one(include_str!("testinput")), 1);
+        assert_eq!(super::part_one(include_str!("testinput")), 24000);
     }
     #[test]
     fn part_two() {
